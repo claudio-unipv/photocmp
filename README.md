40cb4e0f5697a7815259b0dbf95296d88f41dbf2
Claudio Cusano <claudio.cusano@unipv.it>
2022-08-08 16:57:00 +0200

    First working version with MSE and PSNR

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # photocmp
 Compare sets of pictures
+
+
+## Supported metrics
+- [X] MSE
+- [X] PSNR
+- [ ] SSIM
+- [ ] Delta E
+- [ ] LPIPS
+- [ ] Frechet?
---
+++ photocmp/__init__.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def mse(image1, image2):
+    """Mean Squared Error (MSE).
+
+    Note: the function assumes that the depth of both images is 8
+    bits.  The metric is scaled as if input values were in the [0, 1]
+    range.
+
+    """
+    val = ((image1.astype(np.int32) - image2) ** 2).mean()
+    return val / (255 * 255)
+
+
+def psnr(image1, image2):
+    """Peak Signal to Noise Ratio (PSNR)."""
+    mse_val = mse(image1, image2)
+    if mse_val > 0:
+        return -10 * np.log(mse_val)
+    else:
+        return np.inf
