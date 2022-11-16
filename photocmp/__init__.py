5c2d8fc968e6fe10cc3d39da5f9c72322cb4961d
Claudio Cusano <Claudio Cusano>
2022-11-16 09:24:18 +0100

    Stretching and normalization functions

--- photocmp/__init__.py
+++ photocmp/__init__.py
@@ -20,3 +20,20 @@ def psnr(image1, image2):
         return -10 * np.log10(mse_val)
     else:
         return np.inf
+
+
+def stretch(image, eps=1e-6):
+    """Linear scaling of the color values to the [0, 1] range."""
+    num = image - image.min(axis=(0, 1), keepdims=True)
+    den = np.ptp(image, axis=(0, 1), keepdims=True)
+    den = np.clip(den, eps, None)
+    return num / den
+
+
+def normalize(image, eps=1e-6):
+    """Mean-variance normalization."""
+    num = image - image.mean(axis=(0, 1), keepdims=True)
+    den = np.std(image, axis=(0, 1), keepdims=True)
+    den = np.clip(den, eps, None)
+    return num / den
+    
